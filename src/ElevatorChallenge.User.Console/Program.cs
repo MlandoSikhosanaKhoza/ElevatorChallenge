df6d7fa1df9db40131962471768ad9a3fff7cde4
Mlando Sikhosana Khoza <mlando2.0@outlook.com>
2024-06-30 21:10:22 +0200

    Task - The user must be able to input data

--- src/ElevatorChallenge.User.Console/Program.cs
+++ src/ElevatorChallenge.User.Console/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ElevatorChallenge.User.Console.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        IServiceProvider services = ConfigureServices();
+        ElevatorRequestService requestService = services.GetRequiredService<ElevatorRequestService>();
+        string? input = "";
+        do
+        {
+            PrintInstructions();
+            try
+            {
+                Console.WriteLine("Enter the Current Floor, Destination Floor and Passengers in this format:");
+                
+                input = Console.ReadLine();
+                if (input!=null && input.Split(' ').Length==3)
+                {
+                    int currentFloor = Convert.ToInt32(input.Split(" ")[0]);
+                    int destinationFloor = Convert.ToInt32(input.Split(" ")[1]);
+                    int passengers = Convert.ToInt32(input.Split(" ")[2]);
+                    await requestService.RequestElevatorAsync(currentFloor, destinationFloor, passengers);
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid Input - Please read the instructions");
+                PrintInstructions();
+            }
+        } while (!input.ToLower().Equals("q"));
+        Console.ReadLine();
+    }
+
+    private static void PrintInstructions()
+    {
+        Console.WriteLine("Welcome to the Elevator For User");
+        Console.WriteLine("Input the Current Floor, Destination Floor and Passengers in this format:");
+        Console.WriteLine("Example 1: 10 2 6");
+        Console.WriteLine("This represents 10 - Current Floor , 2 - Destination Floor, 6 - Passengers");
+        Console.WriteLine("Example 2: 14 1 4");
+        Console.WriteLine("This represents 14 - Current Floor , 1 - Destination Floor, 4 - Passengers");
+        Console.WriteLine();
+    }
+
+    private static IServiceProvider ConfigureServices()
+    {
+        IServiceCollection services = new ServiceCollection();
+        services.AddSingleton<ElevatorRequestService>();
+        return services.BuildServiceProvider();
+    }
+}
---
+++ src/ElevatorChallenge.User.Console/Services/ElevatorRequestService.cs
@@ -0,0 +1,28 @@
+﻿using ElevatorChallenge.Shared.Models;
+using ElevatorChallenge.User.Console.Constants;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevatorChallenge.User.Console.Services
+{
+    public class ElevatorRequestService
+    {
+        public async Task<bool> RequestElevatorAsync(int currentFloor,int destinationFloor,int passengerCount)
+        {
+            HttpClient client = new HttpClient();
+            string requestUrl = $"{ConsoleConstants.URL}/api/Building/RequestElevator?currentFloor={currentFloor}&destinationFloor={destinationFloor}&passengers={passengerCount}";
+            client.BaseAddress = new Uri(requestUrl);
+            HttpResponseMessage response = await client.GetAsync(requestUrl);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
